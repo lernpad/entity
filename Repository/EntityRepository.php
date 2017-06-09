1e8b2ebbd9c82c047417c86f06e2fc784d99248b
Andrey Chernov <lernpad@mail.ru>
2017-06-10 05:22:39 +0700

    removed findLastOrNew, use findOneOrCreate instead

--- Repository/EntityRepository.php
+++ Repository/EntityRepository.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\EntityRepository as BaseRepository;
 class EntityRepository extends BaseRepository
 {
 
-    public function findOneOrCreate(array $criteria)
+    public function findOneOrCreate(array $criteria, array $orderBy = null)
     {
         $entity = $this->findOneBy($criteria);
         if (null === $entity) {
@@ -17,17 +17,6 @@ class EntityRepository extends BaseRepository
                 $fieldName = lcfirst(\Doctrine\Common\Util\Inflector::classify($key));
                 call_user_func([$entity, 'set' . ucfirst($fieldName)], $value);
             }
-        }
-        return $entity;
-    }
-
-    public function findLastOrNew(array $criteria)
-    {
-        $entity = $this->findOneBy($criteria, ['id' => 'DESC']);
-
-        if (null === $entity) {
-            $class = $this->_class->getName();
-            $entity = new $class;
         }
         return $entity;
     }
